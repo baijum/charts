e8f3b300aaea19473b1e0a2d5a92e2dbe7a56e4d
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-03-18 18:16:53 +0530

    cp

--- src/chartrepomanager/chartrepomanager.py
+++ src/chartrepomanager/chartrepomanager.py
@@ -1,11 +1,24 @@
+import re
+import subprocess
+
 def get_modified_charts():
-    #git diff-tree --no-commit-id --name-only -r `git rev-parse --verify HEAD`
+    commit = subprocess.run(["git", "rev-parse", "--verify", "HEAD"], capture_output=True)
+    commit_hash = commit.stdout.strip()
+    files = subprocess.run(["git", "diff-tree", "--no-commit-id", "--name-only", "-r", commit_hash], capture_output=True)
+    pattern = re.compile("charts/(\w+)/([\w-]+)/([\w-]+)/([\w\.]+)/.*")
+    count = 0
+    for line in files.stdout.decode("utf-8").split('\n'):
+        print(line)
+        m = pattern.match(line)
+        breakpoint()
+        if m:
+            print("hiiiiiiii")
+            cat, org, chart, version = m.groups()
+
+def prepare_chart_for_release():
     pass
 
-def prepare_release():
-    pass
-
-def push_release():
+def push_chart_release():
     pass
 
 def create_index():
@@ -15,4 +28,4 @@ def push_index():
     pass
 
 def main():
-    print("test")
+    get_modified_charts()
