885b0a27296ca1445a99a986836b851871f85f88
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-03-18 14:21:37 +0530

    cp

---
+++ src/chartrepomanager/chartrepomanager.py
@@ -0,0 +1,18 @@
+def get_modified_charts():
+    #git diff-tree --no-commit-id --name-only -r `git rev-parse --verify HEAD`
+    pass
+
+def prepare_release():
+    pass
+
+def push_release():
+    pass
+
+def create_index():
+    pass
+
+def push_index():
+    pass
+
+def main():
+    print("test")
